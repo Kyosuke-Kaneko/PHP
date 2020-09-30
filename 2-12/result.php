df27f81e13c3dce500911ea6491e7ab1fc3bde6a
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2020-09-30 18:04:40 +0900

    2−12
    PHP
    いろいろなフォームの入力欄

---
+++ 2-12/result.php
@@ -0,0 +1,8 @@
+<?php
+$my_name = $_POST['my_name'];
+$choices = $_POST['choices'];
+$number = $_POST['number'];
+?>
+<p>私の名前は、<?php echo $my_name; ?></p>
+<p>ご希望商品<?php echo $choices; ?></p>
+<p>個数は、<?php echo $number; ?></p>
