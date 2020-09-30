dd142f4d1646f34e93a3b928a7163a3922d96a2f
Kyosuke Kaneko <kyosuke.soccer118@gmail.com>
2020-09-30 17:30:50 +0900

    課題2-10
    PHP
    関数

---
+++ 2-10/index.php
@@ -0,0 +1,16 @@
+<?php  
+function getRectangularVolume($vertical, $horizonal, $height) {
+  $volume = $vertical * $horizonal * $height;
+  return $volume;
+}
+echo getRectangularVolume(5,10,8);
+?>
+<p>
+<ul><li>1. ハッシュ化
+ハッシュ関数と呼ばれる特殊な計算手順により、任意長の長さのデータから固定長の一見ランダムに見えるハッシュ値を得ることでパスワードの保管などに用いられる</li>
+<li>2. 総合テスト
+開発の最終段階にシステム全体を対象に行われる本番と同じ環境で行われるテスト</li>
+<li>3. デバッグ
+コンピュータプログラムに潜む欠陥を探し出して取り除くこと</li>
+</ul>
+</p>
